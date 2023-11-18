5f498f8fdc90c1bd2465b2c22d418120db8f5a25
diogoptx <diogommfaria@gmail.com>
2023-11-18 19:38:06 +0000

    sql provisorio

--- RedeSocial.sql
+++ RedeSocial.sql
@@ -1,10 +1,29 @@
-CREATE TABLE IF NOT EXISTS Utilizadores (
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS Mensagens;
+DROP TABLE IF EXISTS Seguidores;
+DROP TABLE IF EXISTS Posts;
+DROP TABLE IF EXISTS Grupo;
+DROP TABLE IF EXISTS GrupoMembros;
+DROP TABLE IF EXISTS GrupoModeradores;
+DROP TABLE IF EXISTS PedidosAdesao;
+DROP TABLE IF EXISTS Partilha;
+DROP TABLE IF EXISTS Gosto;
+DROP TABLE IF EXISTS Comentario;
+DROP TABLE IF EXISTS RecomendacoesPosts;
+DROP TABLE IF EXISTS InteressesUtilizador;
+DROP TABLE IF EXISTS Interesses;
+-- DROP TRIGGER IF EXISTS trigger_grupo_moderadores;
+-- DROP TRIGGER IF EXISTS trigger_pedidos_aceites;
+DROP TABLE IF EXISTS Utilizadores;
+
+CREATE TABLE Utilizadores (
   id integer NOT NULL PRIMARY KEY,
   nomeUtilizador text(20) NOT NULL UNIQUE,
   email text(100) NOT NULL UNIQUE,
   primeiroNome text(255) NOT NULL,
   ultimoNome text(255) NOT NULL,
-  dataNacismento date NOT NULL,
+  dataNascimento date NOT NULL,
   localizacao text(255) NOT NULL,
   descricao text(255) NOT NULL,
   statusVerificado boolean NOT NULL,
@@ -22,24 +41,26 @@ CREATE TABLE IF NOT EXISTS Mensagens (
   FOREIGN KEY (idUtilizadorDestinatario) REFERENCES Utilizadores(id)
 );
 CREATE TABLE IF NOT EXISTS Seguidores (
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     Utilizador integer NOT NULL,
     UtilizadorSeguido integer NOT NULL,
     FOREIGN KEY (Utilizador) REFERENCES Utilizadores(id),
-    FOREIGN KEY (UtilizadorSeguido) REFERENCES Utilizadores(id)
-    PRIMARY KEY(Utilizador, UtilizadorSeguido)
+    FOREIGN KEY (UtilizadorSeguido) REFERENCES Utilizadores(id),
+    UNIQUE(Utilizador, UtilizadorSeguido)
     );
 CREATE TABLE IF NOT EXISTS Posts (
-    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-    visualizacoes integer NOT NULL,
-    idUtilizador integer NOT NULL,
-    idGrupo integer NOT NULL,
-    texto text(255),
-    imagem text(255),
-    video text(255),
-    data datetime DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    visualizacoes INTEGER NOT NULL,
+    idUtilizador INTEGER NOT NULL,
+    idGrupo INTEGER NOT NULL,
+    texto TEXT(255),
+    imagem TEXT(255),
+    video TEXT(255),
+    data DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
+    FOREIGN KEY (idGrupo, idUtilizador) REFERENCES GrupoMembros(idGrupo, idUtilizador),
     FOREIGN KEY (idGrupo) REFERENCES Grupo(id) ON DELETE CASCADE
-    );
+);
 CREATE TABLE IF NOT EXISTS Grupo (
     id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     nome text(255) NOT NULL,
@@ -51,82 +72,88 @@ CREATE TABLE IF NOT EXISTS Grupo (
     FOREIGN KEY (creator) REFERENCES Utilizadores(id)
     );
 CREATE TABLE IF NOT EXISTS GrupoMembros (
-    idGrupo integer NOT NULL,
-    idUtilizador integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    idGrupo INTEGER NOT NULL,
+    idUtilizador INTEGER NOT NULL,
     FOREIGN KEY (idGrupo) REFERENCES Grupo(id) ON DELETE CASCADE,
-    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id)
-    PRIMARY KEY (idGrupo, idUtilizador)
-    );
+    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
+    UNIQUE (idGrupo, idUtilizador)
+);
 CREATE TABLE IF NOT EXISTS GrupoModeradores (
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     idGrupo integer NOT NULL,
     idUtilizador integer NOT NULL,
     FOREIGN KEY (idGrupo) REFERENCES Grupo(id) ON DELETE CASCADE,
-    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id)
-    PRIMARY KEY (idGrupo, idUtilizador)
+    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
+    UNIQUE (idGrupo, idUtilizador)
     );
-CREATE TRIGGER IF NOT EXISTS trigger_grupo_moderadores
+/*CREATE TRIGGER IF NOT EXISTS trigger_grupo_moderadores
     AFTER INSERT ON Grupo
     BEGIN
         INSERT INTO GrupoModeradores (idGrupo, idUtilizador)
         VALUES (NEW.id, NEW.creator);
         INSERT INTO GrupoMembros (idGrupo, idUtilizador)
         VALUES (NEW.id, NEW.creator);
-    END;
+    END;*/
 CREATE TABLE IF NOT EXISTS PedidosAdesao (
-    idGrupo integer NOT NULL,   
-    idUtilizador integer NOT NULL,
-    dataPedido datetime DEFAULT CURRENT_TIMESTAMP,
-    dataAceitacao datetime,
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    idGrupo INTEGER NOT NULL,
+    idUtilizador INTEGER NOT NULL,
+    dataPedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+    dataAceitacao DATETIME,
     FOREIGN KEY (idGrupo) REFERENCES Grupo(id) ON DELETE CASCADE,
     FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
-    PRIMARY KEY (idGrupo, idUtilizador)
-    );
-CREATE TRIGGER IF NOT EXISTS trigger_pedidos_aceites
+    UNIQUE (idGrupo, idUtilizador)
+);
+/*CREATE TRIGGER IF NOT EXISTS trigger_pedidos_aceites
     AFTER UPDATE ON PedidosAdesao
     WHEN PedidosAdesao.dataAceitacao IS NOT NULL
     BEGIN
         INSERT INTO GrupoMembros (idGrupo, idUtilizador)
         VALUES (NEW.idGrupo, NEW.idUtilizador);
-    END;
+    END;*/
 CREATE TABLE IF NOT EXISTS Partilha(
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     idPost integer NOT NULL,   
     idUtilizador integer NOT NULL,
     idUtilizadorDestino integer NOT NULL,
     FOREIGN KEY (idPost) REFERENCES Posts(id)  ON DELETE CASCADE,
-    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id)
+    FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
     FOREIGN KEY (idUtilizadorDestino) REFERENCES Utilizadores(id)
-    PRIMARY KEY (idPost, idUtilizador)
 );
 CREATE TABLE IF NOT EXISTS Gosto(
-    idPost integer NOT NULL,   
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    idPost integer NOT NULL, 
     idUtilizador integer NOT NULL,
     FOREIGN KEY (idPost) REFERENCES Posts(id) ON DELETE CASCADE,
     FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id),
-    PRIMARY KEY (idPost, idUtilizador)
+    UNIQUE (idPost, idUtilizador)
 );
 CREATE TABLE IF NOT EXISTS Comentario(
     id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     idPost integer NOT NULL,   
     idUtilizador integer NOT NULL,
-    dataPedido datetime DEFAULT CURRENT_TIMESTAMP,
+    data datetime DEFAULT CURRENT_TIMESTAMP,
     conteudo text(255) NOT NULL,
     FOREIGN KEY (idPost) REFERENCES Posts(id) ON DELETE CASCADE,
     FOREIGN KEY (idUtilizador) REFERENCES Utilizadores(id)
 );
 CREATE TABLE IF NOT EXISTS Interesses(
-    topico NOT NULL PRIMARY KEY
+    topico TEXT NOT NULL PRIMARY KEY
 );
 CREATE TABLE IF NOT EXISTS RecomendacoesPosts(
-    topicos NOT NULL,
-    post NOT NULL,
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    topicos TEXT NOT NULL,
+    post INTEGER NOT NULL,
     FOREIGN KEY (post) REFERENCES Posts(id) ON DELETE CASCADE,
     FOREIGN KEY (topicos) REFERENCES Interesses(topico),
-    PRIMARY KEY(topicos, post)
+    UNIQUE(topicos, post)
 );
 CREATE TABLE IF NOT EXISTS InteressesUtilizador(
-    topicos NOT NULL,
-    utilizador NOT NULL,
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    topicos TEXT NOT NULL,
+    utilizador INTEGER NOT NULL,
     FOREIGN KEY (utilizador) REFERENCES Utilizadores(id),
     FOREIGN KEY (topicos) REFERENCES Interesses(topico),
-    PRIMARY KEY(topicos, utilizador)
+    UNIQUE(topicos, utilizador)
 );
--- populate.sql
+++ populate.sql
@@ -1,14 +1,237 @@
--- Inserting data into Utilizadores
-INSERT INTO Utilizadores (id, nomeUtilizador, email, primeiroNome, ultimoNome, dataNacismento, localizacao, descricao, statusVerificado, fotoPerfil) VALUES
-(1, 'user1', 'user1@example.com', 'User', 'One', '1990-01-01', 'Porto, Portugal', 'Hello, I am User One!', 1, 'user1.jpg'),
-(2, 'user2', 'user2@example.com', 'User', 'Two', '1992-02-02', 'Lisbon, Portugal', 'Hello, I am User Two!', 0, 'user2.jpg');
+PRAGMA FOREIGN_KEYS=ON;
 
--- Inserting data into Grupo
-INSERT INTO Grupo (id, nome, descricao, foto, creator, statusAcesso) VALUES
-(1, 'Group One', 'This is Group One', 'group1.jpg', 1, 1),
-(2, 'Group Two', 'This is Group Two', 'group2.jpg', 2, 0);
+INSERT INTO Utilizadores (nomeUtilizador, email, primeiroNome, ultimoNome, dataNascimento, localizacao, descricao, statusVerificado, fotoPerfil)
+VALUES
+  ('user1', 'user1@example.com', 'John', 'Doe', '1990-01-01', 'City1', 'Description1', true, 'profile1.jpg'),
+  ('user2', 'user2@example.com', 'Jane', 'Smith', '1985-02-15', 'City2', 'Description2', true, 'profile2.jpg'),
+  ('user3', 'user3@example.com', 'Bob', 'Johnson', '1992-03-22', 'City3', 'Description3', true, 'profile3.jpg'),
+  ('user4', 'user4@example.com', 'Alice', 'Williams', '1988-04-10', 'City4', 'Description4', true, 'profile4.jpg'),
+  ('user5', 'user5@example.com', 'Charlie', 'Brown', '1995-05-05', 'City5', 'Description5', true, 'profile5.jpg'),
+  ('user6', 'user6@example.com', 'Eva', 'Miller', '1980-06-18', 'City6', 'Description6', true, 'profile6.jpg'),
+  ('user7', 'user7@example.com', 'Frank', 'Davis', '1998-07-29', 'City7', 'Description7', true, 'profile7.jpg'),
+  ('user8', 'user8@example.com', 'Grace', 'Moore', '1983-08-14', 'City8', 'Description8', true, 'profile8.jpg'),
+  ('user9', 'user9@example.com', 'Henry', 'Taylor', '1991-09-03', 'City9', 'Description9', true, 'profile9.jpg'),
+  ('user10', 'user10@example.com', 'Ivy', 'Martin', '1987-10-20', 'City10', 'Description10', true, 'profile10.jpg'),
+  ('user11', 'user11@example.com', 'Olivia', 'Anderson', '1989-11-11', 'City11', 'Description11', true, 'profile11.jpg'),
+  ('user12', 'user12@example.com', 'Paul', 'Clark', '1994-12-12', 'City12', 'Description12', true, 'profile12.jpg'),
+  ('user13', 'user13@example.com', 'Quincy', 'Evans', '1986-06-13', 'City13', 'Description13', true, 'profile13.jpg'),
+  ('user14', 'user14@example.com', 'Ruby', 'Fisher', '1993-07-14', 'City14', 'Description14', true, 'profile14.jpg'),
+  ('user15', 'user15@example.com', 'Sam', 'Garcia', '1984-08-15', 'City15', 'Description15', true, 'profile15.jpg'),
+  ('user16', 'user16@example.com', 'Tina', 'Hill', '1997-09-16', 'City16', 'Description16', true, 'profile16.jpg'),
+  ('user17', 'user17@example.com', 'Ulysses', 'Irwin', '1982-10-17', 'City17', 'Description17', true, 'profile17.jpg'),
+  ('user18', 'user18@example.com', 'Victoria', 'Jordan', '1990-11-18', 'City18', 'Description18', true, 'profile18.jpg'),
+  ('user19', 'user19@example.com', 'Walter', 'Kane', '1985-12-19', 'City19', 'Description19', true, 'profile19.jpg'),
+  ('user20', 'user20@example.com', 'Xena', 'Lopez', '1992-01-20', 'City20', 'Description20', true, 'profile20.jpg'),
+  ('user21', 'user21@example.com', 'Yasmine', 'Morales', '1988-02-21', 'City21', 'Description21', true, 'profile21.jpg'),
+  ('user22', 'user22@example.com', 'Zachary', 'Nguyen', '1995-03-22', 'City22', 'Description22', true, 'profile22.jpg'),
+  ('user23', 'user23@example.com', 'Aria', 'Owens', '1980-04-23', 'City23', 'Description23', true, 'profile23.jpg'),
+  ('user24', 'user24@example.com', 'Bryce', 'Perez', '1991-05-24', 'City24', 'Description24', true, 'profile24.jpg'),
+  ('user25', 'user25@example.com', 'Carmen', 'Quinn', '1986-06-25', 'City25', 'Description25', true, 'profile25.jpg'),
+  ('user26', 'user26@example.com', 'Dylan', 'Reyes', '1993-07-26', 'City26', 'Description26', true, 'profile26.jpg'),
+  ('user27', 'user27@example.com', 'Elena', 'Santos', '1982-08-27', 'City27', 'Description27', true, 'profile27.jpg'),
+  ('user28', 'user28@example.com', 'Felix', 'Tran', '1997-09-28', 'City28', 'Description28', true, 'profile28.jpg'),
+  ('user29', 'user29@example.com', 'Gabrielle', 'Underwood', '1984-10-29', 'City29', 'Description29', true, 'profile29.jpg'),
+  ('user30', 'user30@example.com', 'Harvey', 'Vega', '1992-11-30', 'City30', 'Description30', true, 'profile30.jpg'),
+  ('user31', 'user31@example.com', 'Isaac', 'Watson', '1989-12-01', 'City31', 'Description31', true, 'profile31.jpg'),
+  ('user32', 'user32@example.com', 'Jessica', 'Xiong', '1994-01-02', 'City32', 'Description32', true, 'profile32.jpg'),
+  ('user33', 'user33@example.com', 'Kai', 'Yates', '1986-02-03', 'City33', 'Description33', true, 'profile33.jpg'),
+  ('user34', 'user34@example.com', 'Lila', 'Zhang', '1993-03-04', 'City34', 'Description34', true, 'profile34.jpg'),
+  ('user35', 'user35@example.com', 'Mason', 'Adams', '1984-04-05', 'City35', 'Description35', true, 'profile35.jpg'),
+  ('user36', 'user36@example.com', 'Nina', 'Baker', '1997-05-06', 'City36', 'Description36', true, 'profile36.jpg'),
+  ('user37', 'user37@example.com', 'Oscar', 'Chen', '1982-06-07', 'City37', 'Description37', true, 'profile37.jpg'),
+  ('user38', 'user38@example.com', 'Penelope', 'Dixon', '1990-07-08', 'City38', 'Description38', true, 'profile38.jpg'),
+  ('user39', 'user39@example.com', 'Quinn', 'Edwards', '1985-08-09', 'City39', 'Description39', true, 'profile39.jpg'),
+  ('user40', 'user40@example.com', 'Riley', 'Ferguson', '1992-09-10', 'City40', 'Description40', true, 'profile40.jpg'),
+  ('user41', 'user41@example.com', 'Samantha', 'Gordon', '1988-10-11', 'City41', 'Description41', true, 'profile41.jpg'),
+  ('user42', 'user42@example.com', 'Tyler', 'Harrison', '1995-11-12', 'City42', 'Description42', true, 'profile42.jpg'),
+  ('user43', 'user43@example.com', 'Natalie', 'Ingram', '1980-12-13', 'City43', 'Description43', true, 'profile43.jpg'),
+  ('user44', 'user44@example.com', 'Oliver', 'James', '1991-01-14', 'City44', 'Description44', true, 'profile44.jpg'),
+  ('user45', 'user45@example.com', 'Paige', 'Keller', '1986-02-15', 'City45', 'Description45', true, 'profile45.jpg'),
+  ('user46', 'user46@example.com', 'Quentin', 'Lawson', '1993-03-16', 'City46', 'Description46', true, 'profile46.jpg'),
+  ('user47', 'user47@example.com', 'Rachel', 'Manning', '1982-04-17', 'City47', 'Description47', true, 'profile47.jpg'),
+  ('user48', 'user48@example.com', 'Steven', 'Newton', '1997-05-18', 'City48', 'Description48', true, 'profile48.jpg'),
+  ('user49', 'user49@example.com', 'Tiffany', 'Owens', '1984-06-19', 'City49', 'Description49', true, 'profile49.jpg'),
+  ('user50', 'user50@example.com', 'Ulysses', 'Patterson', '1992-07-20', 'City50', 'Description50', true, 'profile50.jpg');
 
--- Inserting data into Posts
-INSERT INTO Posts (id, visualizacoes, idUtilizador, idGrupo, texto, imagem, video) VALUES
-(1, 100, 1, 1, 'Hello, this is my first post!', 'post1.jpg', 'post1.mp4'),
-(2, 200, 2, 2, 'Hello, this is my second post!', 'post2.jpg', 'post2.mp4');
+INSERT INTO Grupo (nome, descricao, foto, creator, statusAcesso)
+VALUES
+  ('Group1', 'Description1', 'group1.jpg', 1, true),
+  ('Group2', 'Description2', 'group2.jpg', 11, true),
+  ('Group3', 'Description3', 'group3.jpg', 21, false),
+  ('Group4', 'Description4', 'group4.jpg', 31, true),
+  ('Group5', 'Description5', 'group5.jpg', 41, false);
+
+INSERT INTO GrupoMembros (idGrupo, idUtilizador)
+SELECT
+    G.id AS idGrupo,
+    U.id AS idUtilizador
+FROM
+    Grupo G
+CROSS JOIN
+    Utilizadores U
+ORDER BY
+    RANDOM()
+LIMIT 50;
+
+-- Insert random messages between users
+INSERT INTO Mensagens (idUtilizador, idUtilizadorDestinatario, mensagem, estadoEnvio, estadoVisualizacao, dataEnvio)
+SELECT
+    U1.id AS idUtilizador,
+    U2.id AS idUtilizadorDestinatario,
+    SUBSTR('Random message ' || RANDOM() || ' between users ' || U1.id || ' and ' || U2.id, 1, 255) AS mensagem,
+    true AS estadoEnvio,
+    false AS estadoVisualizacao,
+    CURRENT_TIMESTAMP AS dataEnvio
+FROM
+    Utilizadores U1
+CROSS JOIN
+    Utilizadores U2
+WHERE
+    U1.id <> U2.id -- Exclude self-messages
+ORDER BY
+    RANDOM()
+LIMIT 100; -- Adjust the limit based on the number of messages you want to generate
+
+INSERT INTO Seguidores (Utilizador, UtilizadorSeguido)
+SELECT
+    U1.id AS Utilizador,
+    U2.id AS UtilizadorSeguido
+FROM
+    Utilizadores U1
+CROSS JOIN
+    Utilizadores U2
+WHERE
+    U1.id <> U2.id -- Exclude self-follows
+ORDER BY
+    RANDOM()
+LIMIT 100; -- Adjust the limit based on the number of follows you want to generate
+INSERT INTO Posts (visualizacoes, idUtilizador, idGrupo, texto, imagem, video)
+SELECT
+    SUBSTR(RANDOM()>=0, 1, 255) AS visualizacoes,
+    U.id AS idUtilizador,
+    G.id AS idGrupo,
+    SUBSTR('Post text ' || RANDOM(), 1, 255) AS texto,
+    SUBSTR('image_url_' || ROUND(RANDOM() * 10) || '.jpg', 1, 255) AS imagem,
+    SUBSTR('video_url_' || ROUND(RANDOM() * 10) || '.mp4', 1, 255) AS video
+FROM
+    Utilizadores U
+JOIN
+    GrupoMembros GM ON U.id = GM.idUtilizador
+JOIN
+    Grupo G ON GM.idGrupo = G.id
+ORDER BY
+    RANDOM()
+LIMIT 30;
+INSERT INTO PedidosAdesao (idGrupo, idUtilizador, dataPedido)
+SELECT
+    G.id AS idGrupo,
+    U.id AS idUtilizador,
+    CURRENT_TIMESTAMP AS dataPedido
+FROM
+    Utilizadores U
+CROSS JOIN
+    Grupo G
+LEFT JOIN
+    GrupoMembros GM ON G.id = GM.idGrupo AND U.id = GM.idUtilizador
+WHERE
+    G.statusAcesso = true
+    AND GM.idGrupo IS NULL -- User is not already a member of the group
+ORDER BY
+    RANDOM()
+LIMIT 20;
+INSERT INTO Partilha (idPost, idUtilizador, idUtilizadorDestino)
+SELECT
+    P.id AS idPost,
+    U.id AS idUtilizador,
+    UD.id AS idUtilizadorDestino
+FROM
+    Posts P
+JOIN
+    Utilizadores U ON P.idUtilizador = U.id
+JOIN
+    Utilizadores UD ON U.id <> UD.id -- Exclude self-shares
+ORDER BY
+    RANDOM()
+LIMIT 20; -- Adjust the limit based on the number of shares you want to generate
+INSERT INTO Gosto (idPost, idUtilizador)
+SELECT
+    P.id AS idPost,
+    U.id AS idUtilizador
+FROM
+    Posts P
+JOIN
+    Utilizadores U ON P.idUtilizador <> U.id -- Exclude self-likes
+ORDER BY
+    RANDOM()
+LIMIT 30;
+INSERT INTO Comentario (idPost, idUtilizador, conteudo, data)
+SELECT
+    P.id AS idPost,
+    U.id AS idUtilizador,
+    SUBSTR('Random comment on post ' || P.id || ' by user ' || U.id, 1, 255) AS conteudo,
+    CURRENT_TIMESTAMP AS data
+FROM
+    Posts P
+JOIN
+    Utilizadores U ON P.idUtilizador <> U.id -- Exclude self-comments
+ORDER BY
+    RANDOM()
+LIMIT 30;
+INSERT INTO Interesses (topico)
+VALUES
+    ('Technology'),
+    ('Science'),
+    ('Programming'),
+    ('Art'),
+    ('Music'),
+    ('Travel'),
+    ('Sports'),
+    ('Food'),
+    ('Fashion'),
+    ('Health'),
+    ('Movies'),
+    ('Books'),
+    ('Fitness'),
+    ('Gaming'),
+    ('Photography'),
+    ('History'),
+    ('Nature'),
+    ('Cooking'),
+    ('DIY'),
+    ('Fashion Design');
+
+INSERT INTO RecomendacoesPosts (topicos, post)
+SELECT
+    I.topico AS topicos,
+    P.id AS post
+FROM
+    Interesses I
+CROSS JOIN
+    Posts P
+WHERE
+    RANDOM() < 0.10 -- 10% chance of having a given interest
+ORDER BY
+    P.id, RANDOM();
+INSERT INTO InteressesUtilizador (topicos, utilizador)
+SELECT
+    I.topico AS topicos,
+    U.id AS utilizador
+FROM
+    Interesses I
+CROSS JOIN
+    Utilizadores U
+WHERE
+    RANDOM() < 0.20 -- 20% chance of having a given interest
+ORDER BY
+    U.id, RANDOM();
+INSERT INTO GrupoModeradores (idGrupo, idUtilizador)
+SELECT
+    G.id AS idGrupo,
+    U.id AS idUtilizador
+FROM
+    Grupo G
+CROSS JOIN
+    Utilizadores U
+ORDER BY
+    RANDOM()
+LIMIT 15;
